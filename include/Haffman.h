8f3b620308e826b2d221c68089cc952ba2a8b398
nik <lesnikowka@mail.ru>
2023-05-21 22:15:22 +0300

    delete extra func

--- include/Haffman.h
+++ include/Haffman.h
@@ -10,18 +10,6 @@ class Haffman {
 public:
 	Haffman() = default;
 
-	void printCodes(std::vector<std::vector<bool>> v) {
-		for (int i = 0; i < v.size(); i++) {
-			if (v[i].size()) {
-				std::cout << (unsigned char)i << ": ";
-				for (auto e : v[i]) {
-					std::cout << e;
-				}
-				std::cout << std::endl;
-			}
-		}
-	}
-
 	TBitField encode(const std::string& data)  {
 		HaffmanTreeCreator hc;
 		std::vector<size_t> freq = collectFrequency(data);
