6fd09edd0da59beddc2fd374b20943202a7128e0
nik <lesnikowka@mail.ru>
2023-07-02 21:48:04 +0300

    add LZ77

---
+++ include/LZ77.h
@@ -0,0 +1,100 @@
+#pragma once
+
+#include <string>
+
+
+class LZ77 {
+public:
+	const size_t SIZE_OF_DICT = 10000;
+	const size_t SIZE_OF_BUF = 50;
+	const size_t MIN_SEQ_SIZE = 13;
+
+	std::string encode(const std::string& data) {
+		std::string resultData;
+
+		long long last_replace = 0;
+
+		resultData += makeTriple(0, 0);
+
+		for (size_t i = 0; i < data.size(); i += SIZE_OF_BUF) {
+			bool concurrency = false;
+
+			for (size_t j = SIZE_OF_BUF; j >= MIN_SEQ_SIZE; j--) {
+				std::string word = data.substr(i + SIZE_OF_DICT, j);
+
+				long long place = find(data, word, i, i + SIZE_OF_DICT);
+
+				if (place != -1) {
+					concurrency = true;
+
+					writeNext(resultData, last_replace, resultData.size() - last_replace);
+
+					last_replace = resultData.size();
+					
+					std::string triple = makeTriple(i + SIZE_OF_DICT - place, j);
+
+					resultData += triple;
+
+					resultData += data.substr(i + SIZE_OF_DICT + j, SIZE_OF_BUF - j);
+				}
+			}
+
+			if (!concurrency) {
+				resultData += data.substr(i + SIZE_OF_DICT, SIZE_OF_BUF);
+			}
+		}
+
+		return resultData;
+	}
+
+	std::string decode() {
+		return std::string();
+	}
+
+private:
+
+	long long find(const std::string& source, const std::string& sub, size_t lbound, size_t rbound) {
+		for (size_t i = lbound; i < rbound - sub.size() + 1; i++) {
+			bool concurrency = true;
+			for (size_t j = 0; j < sub.size(); j++) {
+				if (source[i + j] != sub[j]) {
+					concurrency = false;
+					break;
+				}
+			}
+			if (concurrency) {
+				return i;
+			}
+		}
+		return -1;
+	}
+
+	std::string makeTriple(int back, int n) {
+		std::string triple;
+
+		triple += (char)(back >> 24);
+		triple += (char)((back << 8) >> 24);
+		triple += (char)((back << 16) >> 24);
+		triple += (char)((back << 24) >> 24);
+
+		triple += (char)(n >> 24);
+		triple += (char)((n << 8) >> 24);
+		triple += (char)((n << 16) >> 24);
+		triple += (char)((n << 24) >> 24);
+
+		triple += (char)0;
+		triple += (char)0;
+		triple += (char)0;
+		triple += (char)0;
+
+		return triple;
+	}
+
+	void writeNext(std::string& data, long long last_replace, int next) {
+		data[last_replace + 8] = (char)(next >> 24);
+		data[last_replace + 9] = (char)((next << 8) >> 24);
+		data[last_replace + 10] = (char)((next << 16) >> 24);
+		data[last_replace + 11] = (char)((next << 24) >> 24);
+	}
+
+};
--- samples/samples.cpp
+++ samples/samples.cpp
@@ -2,38 +2,44 @@
 #include "Archiver.h"
 #include "File.h"
 #include <fstream>
+//#include <LZ77.h>
 #include <Unpacker.h>
 
 
 int main() {
 	
-	std::string curdir = "C:/Users/Nikita/Desktop/data/";
-
-	Archiver<RLE> arch;
-
-
-	arch.addFile(curdir + "mir.txt");
-
-	arch.addFile(curdir + "ar.pdf");
-
-	arch.addFile(curdir + "test.docx");
-
-
-
-
-	arch.compress();
-
-	arch.save(curdir + "c/");
-
-	Unpacker<RLE> unp;
-	
-	unp.addFile(curdir + "c/mir.txt.rle");
-	unp.addFile(curdir + "c/ar.pdf.rle");
-	unp.addFile(curdir + "c/test.docx.rle");
+	//std::string curdir = "C:/Users/Nikita/Desktop/data/";
 	//
-	unp.unpack();
+	//Archiver<RLE> arch;
 	//
-	unp.save(curdir + "u/");
+	//
+	//arch.addFile(curdir + "mir.txt");
+	//
+	//arch.addFile(curdir + "ar.pdf");
+	//
+	//arch.addFile(curdir + "test.docx");
+	//
+	//
+	//
+	//
+	//arch.compress();
+	//
+	//arch.save(curdir + "c/");
+	//
+	//Unpacker<RLE> unp;
+	//
+	//unp.addFile(curdir + "c/mir.txt.rle");
+	//unp.addFile(curdir + "c/ar.pdf.rle");
+	//unp.addFile(curdir + "c/test.docx.rle");
+	////
+	//unp.unpack();
+	////
+	//unp.save(curdir + "u/");
+
+	std::string data = "asdajkjkasdasd";
+
+	std::string word = "jkjk";
+
 
 
 }
