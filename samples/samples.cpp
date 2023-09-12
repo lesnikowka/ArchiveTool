26843b7bcf9acf156270c9c696e03bf8685013ba
nik <lesnikowka@mail.ru>
2023-09-12 21:58:01 +0300

    try fix hash-find

--- samples/samples.cpp
+++ samples/samples.cpp
@@ -7,31 +7,33 @@
 #include <ctime>
 
 int main() {
-	std::string dir = "C:/Users/lesni/OneDrive/Рабочий стол/data/";
-	std::string outdir = "C:/Users/lesni/OneDrive/Рабочий стол/data/out/";
-	std::string decdir = "C:/Users/lesni/OneDrive/Рабочий стол/data/dec/";
-	std::string name = "1.txt";
-	std::string name_aft_pack = "1.txt.ajr";
-	std::string pname = "ui.exe";
-	std::string pname_aft_pack = "ui.exe.ajr";
+	std::string dir = "C:/Users/lesni/Desktop/";
+	std::string outdir = "C:/Users/lesni/Desktop/";
+	std::string decdir = "C:/Users/lesni/Desktop/dec/";
+	std::string name = "1.pdf";
+	std::string name_aft_pack = name + ".ajr";
 
 	int start = std::clock();
-	
+
 	Archiver arch;
-	arch.addFile(dir + pname);
+	arch.addFile(dir + name);
 	arch.compress();
 	arch.save(outdir);
 
 	int end = std::clock();
 
 	std::cout << "Time: " << (end - start) / CLOCKS_PER_SEC << " seconds" << std::endl;
-	
+
+	start = std::clock();
+
 	Unpacker unp;
-	unp.addFile(outdir + pname_aft_pack);
+	unp.addFile(outdir + name_aft_pack);
 	unp.unpack();
 	unp.save(decdir);
-	
-	
+
+	end = std::clock();
+
+	std::cout << "Time: " << (end - start) / CLOCKS_PER_SEC << " seconds" << std::endl;
 
 
 }
